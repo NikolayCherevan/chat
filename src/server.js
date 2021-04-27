a706240a684bc3d9de178aba6dbf2c05298b8bfe
krivtsun <krivtsuna@gmail.com>
2021-04-27 19:45:38 +0300

    ok

--- src/server.js
+++ src/server.js
@@ -6,23 +6,31 @@ const Server = require("socket.io");
 const io = new Server(server);
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/index.html');
 });
 let connection = 0;
+let userName = false;
 io.on('connection', (socket) => {
     connection++;
-    io.emit('chat connection',connection);
+    io.emit('chat connection', connection);
+    io.emit('chat userStatus', userName)
     socket.on('disconnect', () => {
         connection--;
-
+        io.emit('chat disconnect', connection);
+        
     });
-  });
+
+    socket.on('chat message', (user, msg) => {
+        io.emit('chat message', user, msg)
+    });
+
+
+
+    socket.on('chat typing', (user, status) => {
+        io.emit('chat typing', user, status)
+    });
+});
 
 server.listen(3000, () => {
-  console.log('listening on *:3000');
+    console.log('listening on *:3000');
 });
-io.on('connection', (socket) => {
-    socket.on('chat message', (user, msg) => {
-        io.emit('chat message',user, msg);
-    });
-  });
