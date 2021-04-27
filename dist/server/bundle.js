3b81ea56a4ee98fa19ec69de201cc27cb2d8116b
krivtsun <krivtsuna@gmail.com>
2021-04-27 17:57:44 +0300

    Removing unuseble code

---
+++ dist/server/bundle.js
@@ -0,0 +1,78 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/server.js":
+/*!***********************!*\
+  !*** ./src/server.js ***!
+  \***********************/
+/***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("const express = __webpack_require__(/*! express */ \"express\");\nconst app = express();\nconst http = __webpack_require__(/*! http */ \"http\");\nconst server = http.createServer(app);\n\napp.get('/', (req, res) => {\n  res.send('<h1>Hello world</h1>');\n});\n\nserver.listen(3000, () => {\n    \n  console.log('listening on *:3000');\n\n});\n\n//# sourceURL=webpack://chat/./src/server.js?");
+
+/***/ }),
+
+/***/ "express":
+/*!**************************!*\
+  !*** external "express" ***!
+  \**************************/
+/***/ ((module) => {
+
+"use strict";
+module.exports = express;
+
+/***/ }),
+
+/***/ "http":
+/*!***********************!*\
+  !*** external "http" ***!
+  \***********************/
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("http");;
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/server.js");
+/******/ 	
+/******/ })()
+;
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = [
+    {
+        name: 'server',
+        entry: './src/server.js',
+        target: 'node',
+        output: {
+            path: __dirname + '/dist/server',
+            filename: 'bundle.js',
+        },
+        externals: ['express']
+    },
+    // {
+    //     name: 'client',
+    //     entry: './src/client.js',
+    //     // target: 'web', // by default
+    //     output: {
+    //         path: __dirname + '/dist/client',
+    //         filename: 'bundle.js',
+    //     },
+    // }
+];
